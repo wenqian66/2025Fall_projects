2d39728939bbf9c5b0ef5b9511db25c205e90f28
wenqian66 <156153284+wenqian66@users.noreply.github.com>
2025-12-16 21:34:58 -0600

    utils update variable

--- utils.py
+++ utils.py
@@ -1,15 +1,6 @@
-from config import DEFAULT_PARAMS
 import numpy as np
 
-def _prepare_params(params, kwargs):
-    if params is None:
-        params = DEFAULT_PARAMS.copy()
-    params = params.copy()
-    params.update(kwargs)
-    return params
-
 def print_results_with_ci(aggregated):
-    """Print results with 95% confidence intervals"""
     for strategy, stats in aggregated.items():
         survival_ci = 1.96 * stats['survival_std'] / np.sqrt(stats['n_trials'])
         wealth_ci = 1.96 * stats['wealth_std'] / np.sqrt(stats['n_trials'])
